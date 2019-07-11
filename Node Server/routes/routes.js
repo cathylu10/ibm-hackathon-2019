468ea4b27d3856b22ea6b604ae26a4230041bdc7
Marc Cardenas <Marc.Cardenas@ibm.com>
2019-07-11 12:04:54 -0500

    Uploading first revision of Server Files

---
+++ Node Server/routes/routes.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var workshopController = require("../controllers/worshopController");
+
+router.get('/api/donors',workshopController.listDonors);
+
+router.post('/api/donors',workshopController.uploadDonor);
+
+router.get('/api/donors/:fname/:lname',workshopController.getDonor);
+
+router.post('/api/donors/:fname/:lname',workshopController.changeDonor);
+
+router.delete('/api/donors/:fname/:lname', workshopController.deleteDonor);
+
+router.get('/api/qdonors',workshopController.queryDonors);
+
+module.exports = router;
