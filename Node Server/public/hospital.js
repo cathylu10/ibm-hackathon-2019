861b87c0eebf7fe7d6bb51f7b805a05dd123e0ad
Catherine-Lu <Catherine.Lu@ibm.com>
2019-07-12 05:36:25 -0500

    added login auth

--- Node Server/public/hospital.js
+++ Node Server/public/hospital.js
@@ -14,12 +14,6 @@ function sendDonorForm() {
     request.send(new FormData(formElement));
 };
 
-function goToDash(response) {
-    if (response.success) {
-        window.location.href = "https://ebd.mybluemix.net/dashboard";
-    }
-};
-
 function sendHospitalForm() {
     var url = "https://ebd.mybluemix.net/api/register/hospital";
     var formElement = document.querySelector("form");
@@ -35,4 +29,33 @@ function sendHospitalForm() {
     request.open("POST", url);
     request.send(new FormData(formElement));
 };
+
+function goToDash(response) {
+    if (response.success) {
+        window.location.href = "https://ebd.mybluemix.net/dashboard";
+    } 
+};
+
+function login() {
+    var url = "https://ebd.mybluemix.net/api/login";
+    var formElement = document.querySelector("form");
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            dashFromLogin(response);
+        } else if (this.readyState == 4 && this.status != 200) {
+            alert("unable to log in");
+        }
+    }
+    request.open("POST", url);
+    request.send(new FormData(formElement));
+}
+
+function dashFromLogin(response) {
+    if (response.success != null && response.success !== undefined) {
+        window.location.href = "https://ebd.mybluemix.net/dashboard";
+    }
+}
+
 
